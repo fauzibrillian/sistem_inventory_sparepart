3dc58adca1ea924c1dc76003663e3f7dea4be581
Fauzi <73748420+fauzibrillian@users.noreply.github.com>
2023-02-25 17:04:34 +0700

    tes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Sistem Inventori Sparepart Mobil'),
 
     /*
     |--------------------------------------------------------------------------

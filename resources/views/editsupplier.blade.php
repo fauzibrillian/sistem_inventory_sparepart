b3b42a663399d3bb84e9c55c355f50a94d44f296
Fauzi <73748420+fauzibrillian@users.noreply.github.com>
2023-03-22 14:38:28 +0700

    tes

--- resources/views/editsupplier.blade.php
+++ resources/views/editsupplier.blade.php
@@ -17,8 +17,8 @@ Coded by www.creative-tim.com
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}">
+  <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     Sistem Inventory Sparepart 
@@ -28,10 +28,10 @@ Coded by www.creative-tim.com
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
   <!-- CSS Files -->
-  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+  <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}"/>
+  <link rel="stylesheet" href="{{asset('assets/css/paper-dashboard.css?v=2.0.1')}}" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
+  <link rel="stylesheet" href="{{asset('assets/demo/demo.css')}}" />
 </head>
 
 <body class="">
@@ -40,7 +40,7 @@ Coded by www.creative-tim.com
       <div class="logo">
         <a href="https://www.creative-tim.com" class="simple-text logo-mini">
           <div class="logo-image-small">
-            <img src="../assets/img/logo-small.png">
+            <img src="{{asset('assets/img/logo-small.png')}}">
           </div>
           <!-- <p>CT</p> -->
         </a>
@@ -179,12 +179,10 @@ Coded by www.creative-tim.com
                             <label for="inputkode">Alamat</label>
                             <input type="text" class="form-control" id="alamat" name="alamat" value="{{old('alamat') ?? $supplier->alamat}}">
                         </div>
-                        <div class="form-row">
-                          <div class="form-group col-md-12">
+                        <div class="form-group col-md-12">
                               <label for="inputtipe">Email</label>
                               <input type="email" class="form-control" id="email" name="email" value="{{old('email') ?? $supplier->email}}">
-                          </div>
-                          </div>
+                        </div>
                         <div class="form-group col-md-12">
                             <label for="inputharga">No.Telpon</label>
                             <input type="text" class="form-control" id="no_telpon" name="no_telpon" value="{{old('no_telpon') ?? $supplier->no_telpon}}">
@@ -203,10 +201,10 @@ Coded by www.creative-tim.com
     </div>
   </div>
   <!--   Core JS Files   -->
-  <script src="../assets/js/core/jquery.min.js"></script>
-  <script src="../assets/js/core/popper.min.js"></script>
-  <script src="../assets/js/core/bootstrap.min.js"></script>
-  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <script src="{{asset('assets/js/core/jquery.min.js')}}"></script>
+  <script src="{{asset('assets/js/core/popper.min.js')}}"></script>
+  <script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
+  <script src="{{asset('assets/js/plugins/perfect-scrollbar.jquery.min.js')}}"></script>
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Chart JS -->
@@ -214,8 +212,8 @@ Coded by www.creative-tim.com
   <!--  Notifications Plugin    -->
   <script src="../assets/js/plugins/bootstrap-notify.js"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-  <script src="../assets/demo/demo.js"></script>
+  <script src="{{asset('assets/js/paper-dashboard.min.js?v=2.0.1')}}" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+  <script src="{{asset('assets/demo/demo.js')}}"></script>
   <script>
     $(document).ready(function() {
       // Javascript method's body can be found in assets/assets-for-demo/js/demo.js
